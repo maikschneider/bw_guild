070186b3ae5ff3462f36513f3ef8485141f479ca
Maik Schneider <m.schneider@blueways.de>
2019-07-02 09:44:39 +0200

    fix: add password_repeat field to search exclude field, use correct Demand object in search

--- Classes/Domain/Model/Dto/UserDemand.php
+++ Classes/Domain/Model/Dto/UserDemand.php
@@ -4,4 +4,6 @@ namespace Blueways\BwGuild\Domain\Model\Dto;
 
 class UserDemand extends BaseDemand
 {
+
+    CONST EXCLUDE_FIELDS = 'pid,lockToDomain,image,lastlogin,uid,_localizedUid,_languageUid,_versionedUid,passwordRepeat';
 }
