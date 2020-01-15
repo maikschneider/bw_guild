a69782267227ad8953150ca9875156c5c464cb55
Maik Schneider <m.schneider@blueways.de>
2020-01-15 10:42:33 +0100

    feat: use username in tca label

--- Classes/Utility/LabelUtility.php
+++ Classes/Utility/LabelUtility.php
@@ -7,7 +7,7 @@ class LabelUtility
     public function feUserLabel(&$parameters, $parentObject) {
         $record = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecord($parameters['table'], $parameters['row']['uid']);
         $newTitle = $record['company'];
-        $newTitle .= ' (' . $record['member_nr'] . ')';
+        $newTitle .= ' (' . $record['username'] . ')';
         $parameters['title'] = $newTitle;
     }
 }
