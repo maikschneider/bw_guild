9bece9b75981455b66f84dd0cc8d92cafd9aaea2
Maik Schneider <m.schneider@blueways.de>
2021-04-27 18:52:59 +0200

    feat: add template selection to offer list plugin

--- Classes/Controller/OfferController.php
+++ Classes/Controller/OfferController.php
@@ -54,6 +54,7 @@ class OfferController extends ActionController
         $metaTagManager = GeneralUtility::makeInstance(MetaTagManagerRegistry::class);
         $metaTagManager->getManagerForProperty('robots')->addProperty('robots', 'noindex, follow');
 
+        $this->view->setTemplate($this->settings['template']);
         $this->view->assign('offers', $offers);
     }
 
