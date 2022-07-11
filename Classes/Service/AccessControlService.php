0003e6aa50f7f9bb7e679ea5404bb887bb67ca65
Maik Schneider <mas@xima.de>
2022-07-11 09:21:50 +0200

    chore: refactor AccessControlService

--- Classes/Service/AccessControlService.php
+++ Classes/Service/AccessControlService.php
@@ -3,15 +3,11 @@
 namespace Blueways\BwGuild\Service;
 
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Extbase\Domain\Model\FrontendUser;
 
 class AccessControlService implements SingletonInterface
 {
-
-    /**
-     * @param \TYPO3\CMS\Extbase\Domain\Model\FrontendUser $user
-     * @return bool
-     */
-    public function isLoggedIn($user = null)
+    public function isLoggedIn(?FrontendUser $user = null): bool
     {
         if (is_object($user)) {
             if ($user->getUid() === $this->getFrontendUserUid()) {
@@ -21,23 +17,16 @@ class AccessControlService implements SingletonInterface
         return false;
     }
 
-    /**
-     * @return int|null
-     */
-    public function getFrontendUserUid()
+    public function getFrontendUserUid(): ?int
     {
         if ($this->hasLoggedInFrontendUser() && !empty($GLOBALS['TSFE']->fe_user->user['uid'])) {
-            return intval($GLOBALS['TSFE']->fe_user->user['uid']);
+            return (int)($GLOBALS['TSFE']->fe_user->user['uid']);
         }
         return null;
     }
 
-    /**
-     * @return bool
-     */
-    public function hasLoggedInFrontendUser()
+    public function hasLoggedInFrontendUser(): bool
     {
         return !empty($GLOBALS['TSFE']->fe_user->user);
     }
-
 }
